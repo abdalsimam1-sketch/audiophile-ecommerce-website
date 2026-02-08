1366b7db7fe24fa1649402b29cf27156b95d1bee
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-02-08 15:28:04 +0100

    feat:gallery details section

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -14,14 +14,13 @@ export const Details = () => {
     );
   }
   return (
-    <main>
+    <main className="d-flex flex-column gap-4">
       <div className="container">
         <span onClick={() => navigate(-1)} className="btn">
           Go Back
         </span>
       </div>
-
-      <section className="details-section container d-flex flex-column flex-md-row  text-md-row text-md-row gap-5">
+      <section className="details-section container d-flex flex-column flex-md-row justify-content-between  text-md-row text-md-row gap-5">
         <div>
           <img
             src={filteredProduct.image.mobile}
@@ -44,6 +43,49 @@ export const Details = () => {
               <button className="btn button1">ADD TO CART</button>
             </div>
           </div>
+        </div>
+      </section>
+      <article className="px-3">
+        <section className="container features-section d-flex flex-column flex-md-row gap-md-5 ">
+          <div>
+            <h2>features</h2>
+            <p className="text-muted"> {filteredProduct.features}</p>
+          </div>
+          <div className=" fw-bold">
+            <h2>in the box</h2>
+            {filteredProduct.includes.map((item, index) => (
+              <p key={index}>
+                <span className="overline fw-bold">X{item.quantity}</span>
+                {item.item}
+              </p>
+            ))}
+          </div>
+        </section>
+      </article>
+
+      <section className="container gallery-section d-flex flex-column flex-md-row gap-3">
+        <div className="d-flex flex-column gap-3">
+          <div className=" flex-grow-1">
+            <img
+              src={filteredProduct.gallery.first.mobile}
+              alt=""
+              className="rounded img-fluid h-100 w-100  "
+            />
+          </div>
+          <div className=" flex-grow-1">
+            <img
+              src={filteredProduct.gallery.second.mobile}
+              alt=""
+              className="rounded img-fluid h-100 w-100 "
+            />
+          </div>
+        </div>
+        <div style={{ overflow: "hidden" }}>
+          <img
+            src={filteredProduct.gallery.third.mobile}
+            alt=""
+            className="rounded img-fluid h-100 w-100 "
+          />
         </div>
       </section>
     </main>
