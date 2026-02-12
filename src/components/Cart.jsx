9894ebef1ab8f82323f719af776b3c980aff0b04
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-02-12 16:33:35 +0100

    cart responsiveness test 2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,48 +4,50 @@ import { QuantitySelector } from "../components/QuantitySelector";
 export const Cart = () => {
   const { cart, increase, decrease, clear } = useCart();
   return (
-    <main
-      className=" me-md-5 modal-section  rounded shadow p-5 d-flex flex-column position-fixed bg-light "
-      style={{ zIndex: "9999" }}
-    >
-      <div className="d-flex justify-content-between ">
-        <h4>cart ({cart.length})</h4>
-        <button className="btn" onClick={clear}>
-          Remove All{" "}
-        </button>
-      </div>
+    <div className="d-flex flex-column align-items-center ">
+      <main
+        className="  me-md-5 modal-section  rounded shadow  d-flex flex-column  position-fixed bg-light p-5 "
+        style={{ zIndex: "9999" }}
+      >
+        <div className="d-flex justify-content-between ">
+          <h4>cart ({cart.length})</h4>
+          <button className="btn" onClick={clear}>
+            Remove All{" "}
+          </button>
+        </div>
 
-      <section className="product-details">
-        <div>
-          {cart.map((item) => (
-            <div
-              key={item.id}
-              className="d-flex justify-content-between align-items-center mb-3"
-            >
-              <div className="d-flex align-items-center gap-3 flex-nowrap">
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  style={{ width: "5rem" }}
-                  className="rounded"
-                />
-                <div>
-                  <span className="subtitle">{item.name}</span>
-                  <span>{item.price}</span>
+        <section className="product-details">
+          <div>
+            {cart.map((item, index) => (
+              <div
+                key={item.id}
+                className="d-flex justify-content-between align-items-center mb-3"
+              >
+                <div className="d-flex align-items-center gap-3 flex-nowrap">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    style={{ width: "5rem" }}
+                    className="rounded"
+                  />
+                  <div>
+                    <span className="subtitle">{item.name}</span>
+                    <span>{item.price}</span>
+                  </div>
+                </div>
+                <div className="flex-nowrap">
+                  <QuantitySelector
+                    handleAdd={increase}
+                    handleRemove={decrease}
+                    addBtn={false}
+                    count={item.quantity}
+                  ></QuantitySelector>
                 </div>
               </div>
-              <div className="flex-nowrap">
-                <QuantitySelector
-                  handleAdd={increase}
-                  handleRemove={decrease}
-                  addBtn={false}
-                  count={item.quantity}
-                ></QuantitySelector>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    </main>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
